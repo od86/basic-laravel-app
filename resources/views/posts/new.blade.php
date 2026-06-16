65bbba290a8f328ca10de9297e17c6445bfeb25b
od86 <178620777+od86@users.noreply.github.com>
2026-06-16 08:44:30 +0100

    Created new post page

---
+++ resources/views/posts/new.blade.php
@@ -0,0 +1,20 @@
+<x-layout>
+  <h2>Create Post</h2>
+  <form action="/posts" method="post">
+    @csrf
+
+    <div class="field">
+      <label for="title">Post Title</label>
+      <input type="text" name="title" id="title">
+    </div>
+
+    <div class="field">
+      <label for="body">Post Body</label>
+      <textarea name="body" id="body" cols="25" rows="5"></textarea>
+    </div>
+
+    <div class="action">
+      <button type="submit">Create post</button>
+    </div>
+  </form>
+</x-layout>
