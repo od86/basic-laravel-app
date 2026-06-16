8b9f1eed76dd3ba358fdc9750d6fc4ed3d0fc236
od86 <178620777+od86@users.noreply.github.com>
2026-06-16 08:13:49 +0100

    Created RESTful routes for PostController

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
+use App\Model\PostController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get("/posts", [PostController::class, "index"]);
+Route::get("/posts/new", [PostController::class, "create"]);
+Route::post("/posts", [PostController::class, "store"]);
+Route::get("/posts/{id}", [PostController::class, "show"]);
+Route::get("/posts/{id}/edit", [PostController::class, "edit"]);
+Route::patch("/posts/{id}", [PostController::class, "update"]);
+Route::delete("/posts/{id}", [PostController::class, "destroy"]);
