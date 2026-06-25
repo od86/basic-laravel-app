37e012942a9d1a73f634dafcca6db2a08cb18e59
od86 <178620777+od86@users.noreply.github.com>
2026-06-25 21:58:23 +0100

    Added register page

---
+++ resources/views/auth/register.blade.php
@@ -0,0 +1,20 @@
+<x-layout>
+  <form action="/register" method="post">
+    <div class="field">
+      <label for="name">Name</label>
+    <input type="text" name="name" id="name" required>
+  </div>
+    <div class="field">
+      <label for="email">Email</label>
+    <input type="email" name="email" id="email" required>
+  </div>
+    <div class="field">
+      <label for="password">Password</label>
+    <input type="password" name="password" id="password" min="5" required>
+  </div>
+
+    <div class="action">
+      <button type="submit">Register</button>
+    </div>
+  </form>
+</x-layout>
