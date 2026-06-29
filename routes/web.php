6141e64a1d8785427cd96de4488831066bb2c0db
od86 <178620777+od86@users.noreply.github.com>
2026-06-29 17:35:59 +0100

    Added routes for login and logout

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Auth\SessionsController;
 use App\Http\Controllers\Auth\RegisteredUserController;
 use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
@@ -12,5 +13,13 @@ Route::get("/posts/{post}/edit", [PostController::class, "edit"]);
 Route::patch("/posts/{post}", [PostController::class, "update"]);
 Route::delete("/posts/{post}", [PostController::class, "destroy"]);
 
+// Routes for registering an account
 Route::get("/register", [RegisteredUserController::class, "create"]);
 Route::post("/register", [RegisteredUserController::class, "store"]);
+
+// Routes for login
+Route::get("/login", [SessionsController::class, "create"]);
+Route::post("/login", [SessionsController::class, "store"]);
+
+// Route for logout
+Route::delete("/logout", [SessionsController::class, "destroy"]);
