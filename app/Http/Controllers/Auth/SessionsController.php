5979c12d694b1a0a9afcaa5acb094f36b8b0840c
od86 <178620777+od86@users.noreply.github.com>
2026-06-30 15:01:22 +0100

    Added and improved validation for login/logout

--- app/Http/Controllers/Auth/SessionsController.php
+++ app/Http/Controllers/Auth/SessionsController.php
@@ -23,7 +23,7 @@ class SessionsController extends Controller
     {
         $validated = $request->validate([
             "email" => ["required", "email", "string"],
-            "password" => ["required", "min:5"]
+            "password" => ["required", "string", "min:8"]
         ]);
 
         Auth::attempt($validated);
