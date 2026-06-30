3490d13ab7989ac934096af55ce3761c61df5bf9
od86 <178620777+od86@users.noreply.github.com>
2026-06-30 14:48:29 +0100

    Created SessionsController which handles login/logoutP

---
+++ app/Http/Controllers/Auth/SessionsController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class SessionsController extends Controller
+{
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view("auth/login");
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $validated = $request->validate([
+            "email" => ["required", "email", "string"],
+            "password" => ["required", "min:5"]
+        ]);
+
+        Auth::attempt($validated);
+
+        return redirect("/posts");
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy()
+    {
+        Auth::logout();
+        
+        return redirect("/posts");
+    }
+}
